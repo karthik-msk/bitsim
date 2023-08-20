1cc15f70c8f900abc8d984c09690135447f5c2c4
karthik <mskarthikeyan16@gmail.com>
2023-08-20 12:21:03 +0530

    variable correction

--- mine.py
+++ mine.py
@@ -3,17 +3,18 @@ import shutil
 import time
 import os
 import sys
+
 txs = []
 blocks = []
 tx_per_block = 50
 prev_hash = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 total_blocks = 0
-difficulty = 1
+difficulty = 4
 
-if(os.path.exists("blocks")):
-    shutil.rmtree("blocks")
+if(os.path.exists("chain")):
+    shutil.rmtree("chain")
 
-os.makedirs("blocks")
+os.makedirs("chain")
 
 def hash256(string):
     return hashlib.sha256(string.encode("utf-8")).hexdigest()
@@ -21,32 +22,32 @@ def hash256(string):
 def merkleTree(hashlist):
     if len(hashlist)%2==1:
         hashlist.append(hashlist[-1])
-    print(hashlist)
+    #print(hashlist)
     newlist = []
     for i in range(0, len(hashlist), 2):
         newlist.append(hashlist[i]+hashlist[i+1])
     #print(newlist)
     hashlist = []
     hashlist = [hash256(i) for i in newlist]
-    print(hashlist)
+    #print(hashlist)
     if len(hashlist) == 1:
         return(hashlist[0])
     return(merkleTree(hashlist))
 
-def create_file(total_blocks, hash, content):
-    filename = str(total_blocks) + "-" + hash + ".txt"
+def create_file(total_blocks, hash_value, content):
+    filename = str(total_blocks) + "-" + hash_value + ".txt"
     blocks.append(filename)
-    with open("blocks/"+filename, "w") as file:
+    with open("chain/"+filename, "w") as file:
         file.write(content)
     
 def verify():
-    print("Verifying validity of the chain...")
+    print("\nVerifying validity of the chain...")
     if(len(blocks) > 1):
         for i in range(len(blocks)-1):
-            with open("blocks/"+blocks[i], "r") as file:
+            with open("chain/"+blocks[i], "r") as file:
                 content = file.read()
                 curHash = hash256(content)
-            with open("blocks/"+blocks[i+1], "r") as file:
+            with open("chain/"+blocks[i+1], "r") as file:
                 lines = file.readlines()
                 prevHashLine = lines[1]
                 prevHash = prevHashLine.split(" : ")[1].replace("\n","")
@@ -56,8 +57,8 @@ def verify():
             if(curHash!=prevHash):
                 print("BLOCK NUMBER : ", i," HAS BEEN TAMPERED. CHAIN COLLAPSED")
                 sys.exit()
-
-    print("The chain is VALID")
+    time.sleep(0.5)
+    print("CHAIN IS VALID")
     time.sleep(1)
 
 def mine(transactions, difficulty, merkleroot):
@@ -71,7 +72,7 @@ def mine(transactions, difficulty, merkleroot):
     while hash_value[0:difficulty] != "0"*difficulty:
         #print(block)
         print(hash_value, new_nonce)
-        print("-------------------------------------------")
+        print("-----------------------------------------------------------------------------")
         lines = block.split("\n")
         cur_nonce = lines[3]
         cur_value = int(cur_nonce.split(":")[1])
@@ -81,7 +82,8 @@ def mine(transactions, difficulty, merkleroot):
         hash_value = hash256(block)
     total_blocks = total_blocks + 1
     prev_hash = hash_value
-    print("-MINED", block, hash_value)
+    print("MINED BLOCK : ", hash_value)
+    print("With nonce value : ", new_value)
     create_file(total_blocks-1, hash_value, block)
     #print(blocks)
 
@@ -91,19 +93,18 @@ while True:
         with open("unconfirmed_transactions.txt", "r") as file:
             con = file.readlines()
             if(len(con)>tx_per_block):
-                current_transactions = con[:tx_per_block]
-                remaining = con[tx_per_block:]
+                transactions_to_verify = con[:tx_per_block]
+                remaining_transactions = con[tx_per_block:]
                 break
     
-    with open("unconfirmed_transactions.txt", "w") as rewrite:
-        rewrite.writelines(remaining)
-    hashlist = [hash256(i) for i in current_transactions]
+    with open("unconfirmed_transactions.txt", "w") as file:
+        file.writelines(remaining_transactions)
+    
+    hashlist = [hash256(i) for i in transactions_to_verify]
     merkleroot = merkleTree(hashlist)
 
-
-    mine(current_transactions, difficulty, merkleroot)
+    mine(transactions_to_verify, difficulty, merkleroot)
     verify()
 
-    
-    with open("confirmed.txt", "a+") as update:
-        update.writelines(current_transactions)
+    with open("confirmed.txt", "a+") as file:
+        file.writelines(transactions_to_verify)
